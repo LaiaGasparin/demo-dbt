b4fa9d19ef67c49c1cdb66085403a242b6fd42f7
laia.gasparin@gmail.com <laia.gasparin@gmail.com>
2025-09-26 15:42:33 +0000

    error

--- models/bronze/stg_lineitem.sql
+++ models/bronze/stg_lineitem.sql
@@ -5,7 +5,7 @@ select
   l_partkey         as part_key,
   l_suppkey         as supplier_key,
   l_linenumber      as line_number,
-  cast(l_quantity as number(18,4)       as quantity,
+  cast(l_quantity as number(18,4))       as quantity,
   l_extendedprice   as extended_price,
   l_discount        as discount
 from {{ source('tpch','lineitem') }}
