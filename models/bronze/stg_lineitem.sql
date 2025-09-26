fe45e51be079b1eeed7be0a34d08d3561b7feb93
laia.gasparin@gmail.com <laia.gasparin@gmail.com>
2025-09-26 15:55:40 +0000

    fix error

--- models/bronze/stg_lineitem.sql
+++ models/bronze/stg_lineitem.sql
@@ -1,11 +1,11 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
 select
-  l_orderkey        as order_key,
-  l_partkey         as part_key,
-  l_suppkey         as supplier_key,
-  l_linenumber      as line_number,
-  cast(l_quantity as number(18,4))       as quantity,
-  l_extendedprice   as extended_price,
-  l_discount        as discount
-from {{ source('tpch','lineitem') }}
+    l_orderkey as order_key,
+    l_partkey as part_key,
+    l_suppkey as supplier_key,
+    l_linenumber as line_number,
+    l_quantity as quantity,
+    l_extendedprice as extended_price,
+    l_discount as discount
+from {{ source("tpch", "lineitem") }}
